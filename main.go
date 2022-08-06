6e80364ec408b10c8fc9ebea02f3f38fd2729602
Hari <hari@cloudstick1.com>
2022-08-06 15:42:47 +0530

    added changes in port

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
+
 	//"test/hari/model"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("go running in local...")
-	//model.UpdateStatus()
-	//url := "http://www.google.com"
-	//model.CallApiInGo(url)
-	//model.SendMail()
-	http.HandleFunc("/demo", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello This is hari"))
-	})
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	log.Print("Lisitening on Port: ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, This is hari...")
 }

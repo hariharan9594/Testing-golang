d2e9997cac5b04377e04a5dc6aec79af740f2538
Hari <hari@cloudstick1.com>
2022-08-06 16:25:08 +0530

    post func added

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/post", post)
+
 	log.Print("Lisitening on Port: ", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, This is hari...")
+	io.WriteString(w, "Hello, This is hari..Nice to see you...")
+}
+func post(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "This page gives u details about live market...")
 }
